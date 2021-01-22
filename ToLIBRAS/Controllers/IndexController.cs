22cdea47e09a2a9b8a4be5613e0897f248514fc2
ARTHUR DE MELO GALVÃO <arthur.galvao@academico.ifrn.edu.br>
2021-01-22 18:23:35 -0300

    Página registro.html alterada com ASP.NET MVC

--- ToLIBRAS/Controllers/IndexController.cs
+++ ToLIBRAS/Controllers/IndexController.cs
@@ -13,5 +13,9 @@ namespace ToLIBRAS.Controllers
         {
             return View();
         }
+        public ActionResult Registro ()
+        {
+            return View();
+        }
     }
 }
